1a2c402e0291b6a8ca17505cb3616beef673e495
Xiang <xdong57@emory.edu>
2020-04-09 00:31:07 -0400

    add code

---
+++ run_bert_finetune.sh
@@ -0,0 +1,17 @@
+python3 run_glue.py \
+  --model_type bert \
+  --model_name_or_path bert-base-uncased \
+  --task_name $TASK_NAME \
+  --do_train \
+  --do_eval \
+  --eval_all_checkpoints \
+  --evaluate_during_training \
+  --do_lower_case \
+  --data_dir $GLUE_DIR/$TASK_NAME \
+  --max_seq_length 128 \
+  --per_gpu_train_batch_size 64 \
+  --per_gpu_eval_batch_size 64 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 10.0 \
+  --save_steps 5000 \
+  --output_dir bert_fine_tune_output
